0fd86cbcb5be897111b058584232a303b56488f7
chou0728 <dean34520@gmail.com>
2020-07-21 18:04:19 +0800

    引入jquery做收放功能

--- src/components/Treant.vue
+++ src/components/Treant.vue
@@ -5,20 +5,24 @@
 <script>
 // http://fperucic.github.io/treant-js/
 import "treant-js/Treant.css";
-import "treant-js/examples/basic-example/basic-example.css";
 import Raphael from "raphael";
+import $ from "jquery";
 
 export default {
   mounted() {
     window.Raphael = Raphael;
+    window.$ = $;
     let Treant = require("treant-js/Treant");
     Treant = Treant.Treant;
     new Treant({
       chart: {
         container: "#tree-simple",
-      },
-      connectors: {
-        type: "step",
+        connectors: {
+          type: "step",
+        },
+        node: {
+          collapsable: true,
+        },
       },
       nodeStructure: {
         text: {
@@ -28,26 +32,46 @@ export default {
         },
         children: [
           {
-            image:
-              "http://flat-icon-design.com/f/f_object_167/s256_f_object_167_0bg.png",
+            text: {
+              name: "Simple node",
+              title: "One of kind",
+              desc: "A basic example",
+            },
           },
           {
-            image:
-              "http://flat-icon-design.com/f/f_object_80/s256_f_object_80_0bg.png",
+            text: {
+              name: "Simple node",
+              title: "One of kind",
+              desc: "A basic example",
+            },
+          },
+          {
+            text: {
+              name: "Simple node",
+              title: "One of kind",
+              desc: "A basic example",
+            },
+          },
+          {
+            text: {
+              name: "Simple node",
+              title: "One of kind",
+              desc: "A basic example",
+            },
             children: [
               {
-                image:
-                  "http://flat-icon-design.com/f/f_object_78/s256_f_object_78_0bg.png",
+                text: {
+                  name: "Simple node",
+                  title: "One of kind",
+                  desc: "A basic example",
+                },
               },
               {
-                image:
-                  "http://flat-icon-design.com/f/f_object_114/s256_f_object_114_0bg.png",
-                children: [
-                  {
-                    image:
-                      "http://flat-icon-design.com/f/f_object_67/s256_f_object_67_0bg.png",
-                  },
-                ],
+                text: {
+                  name: "Simple node",
+                  title: "One of kind",
+                  desc: "A basic example",
+                },
               },
             ],
           },
@@ -64,11 +88,21 @@ export default {
   height: 100vh;
   margin: 5px;
   .node {
-    border: 1px solid red;
-    padding: 0 20px;
+    border: 1px solid black;
+    max-width: 200px;
+    padding: 0 10px;
+    text-align: center;
   }
   .node-name {
     color: red;
+  }
+  .collapse-switch {
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    border: none;
+    background-color: transparent;
   }
 }
 </style>
